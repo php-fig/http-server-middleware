32a7ba536230b70625fcb45ec4677732b9367789
Matthieu Napoli <matthieu@mnapoli.fr>
2016-08-17 19:49:04 +0200

    Add interfaces from the current draft of PSR-15

---
+++ src/MiddlewareInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Psr\Http\Middleware;
+
+interface MiddlewareInterface
+{
+}
---
+++ src/ServerMiddlewareInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Psr\Http\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+interface ServerMiddlewareInterface extends MiddlewareInterface
+{
+    /**
+     * Process a server request and return a response.
+     *
+     * Takes the incoming request and optionally modifies it before delegating
+     * to the next frame to get a response.
+     *
+     * @param ServerRequestInterface $request
+     * @param FrameInterface $frame
+     *
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, FrameInterface $frame);
+}
